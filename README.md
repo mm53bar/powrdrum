f6d993653206b1fe4aca4b2db3739a35694b2531
Jordan <jorbabe@gmail.com>
2011-08-02 13:44:22 -0600

    Rubifying it

--- README.md
+++ README.md
@@ -4,4 +4,4 @@ Run the following commands:
 
 `sh -c 'mkdir -p /usr/local/src/workstation_bootstrap_tmp && cd /usr/local/src/workstation_bootstrap_tmp && curl -L https://github.com/jorbabe/workstation_bootstrap/tarball/master | gunzip | tar xvf - --strip=1'`
 
-`cd /usr/local/src/workstation_bootstrap_tmp && chmod +x bootstrap.sh && ./bootstrap.sh`
+`cd /usr/local/src/workstation_bootstrap_tmp && ruby bootstrap.rb`
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,11 @@
-curl -O http://files.rubyforge.vm.bytemark.co.uk/rubygems/rubygems-1.6.2.tgz
-tar xvzf rubygems-1.6.2.tgz
-cd rubygems-1.6.2 && ruby setup.rb
-gem install soloist --no-ri --no-rdoc
-gem install bundler --no-ri --no-rdoc
-mkdir -p chef/cookbooks/pivotal_workstation && cd chef/cookbooks/pivotal_workstation && curl -L http://github.com/jorbabe/pivotal_workstation/tarball/master | gunzip | tar xvf - --strip=1
-soloist
+`sh -c 'mkdir -p /usr/local/src/workstation_bootstrap_tmp && cd /usr/local/src/workstation_bootstrap_tmp && curl -L https://github.com/jorbabe/workstation_bootstrap/tarball/master | gunzip | tar xvf - --strip=1'`
+
+`cd /usr/local/src/workstation_bootstrap_tmp && chmod +x bootstrap.sh && ./bootstrap.sh`
+
+`which gem`
+if $? != 0
+	`curl -O http://files.rubyforge.vm.bytemark.co.uk/rubygems/rubygems-1.6.2.tgz && tar xvzf rubygems-1.6.2.tgz && cd rubygems-1.6.2 && ruby setup.rb`
+	`gem install soloist --no-ri --no-rdoc && gem install bundler --no-ri --no-rdoc`
+	`mkdir -p chef/cookbooks/pivotal_workstation && cd chef/cookbooks/pivotal_workstation && curl -L http://github.com/jorbabe/pivotal_workstation/tarball/master | gunzip | tar xvf - --strip=1`
+	`soloist`
+end
