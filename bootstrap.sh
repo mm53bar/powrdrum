23f12d9dafbe8a299df21de01e7eb1631f551a7d
Jordan Babe <jordan.babe@ama.ab.ca>
2011-08-05 11:49:26 -0600

    Reverting to a shell script

---
+++ bootstrap.sh
@@ -0,0 +1,12 @@
+sh -c 'mkdir -p chef/cookbooks/pivotal_workstation && cd chef/cookbooks/pivotal_workstation && curl -Ls http://github.com/jorbabe/pivotal_workstation/tarball/master | gunzip | tar xvf - --strip=1'
+which gem
+OUT=$?
+if [ $OUT -ne 0 ];then
+  curl -LOs http://production.cf.rubygems.org/rubygems/rubygems-1.6.2.tgz
+  tar zxvf rubygems-1.6.2.tgz && cd rubygems-1.6.2 && ruby setup.rb
+else
+   echo "Rubygems already installed...skipping..."
+fi
+gem install soloist --no-ri --no-rdoc && gem install bundler --no-ri --no-rdoc
+soloist
+say 'Now go buy Jordan a coffee and go write some code!'
