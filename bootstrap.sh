1c90695b619ad9b912e4a51826937a2b865dfde0
Jordan Babe <jordan.babe@ama.ab.ca>
2011-08-05 13:36:37 -0600

    Forgot soloist file

--- bootstrap.sh
+++ bootstrap.sh
@@ -11,12 +11,13 @@ sh -c 'mkdir -p chef/cookbooks/pivotal_workstation && cd chef/cookbooks/pivotal_
 which gem
 OUT=$?
 if [ $OUT -ne 0 ];then
+  echo "Installing Rubygems..."
   curl -LOs http://production.cf.rubygems.org/rubygems/rubygems-1.6.2.tgz
   tar zxvf rubygems-1.6.2.tgz && cd rubygems-1.6.2 && ruby setup.rb
 else
    echo "Rubygems already installed...skipping..."
 fi
 gem install soloist --no-ri --no-rdoc && gem install bundler --no-ri --no-rdoc
-soloist
-open https://github.com/jorbabe/workstation_bootstrap/raw/master/drummer.gif
+curl -O https://raw.github.com/jorbabe/powrdrum/master/soloistrc && soloist
+open https://github.com/jorbabe/powrdrum/raw/master/drummer.gif
 say -v Alex Dum dum dee dum dum dum dum dee Dum dum dee dum dum dum dum dee dum dee dum dum dum de dum dum dum dee dum dee dum dum dee dummmmmmmmmmmmmmmmm
